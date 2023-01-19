159a6f56497125d23003319d154a07cf0f13849c
James Sangalli <j.l.sangalli@gmail.com>
2023-01-19 14:43:58 +1100

    fix bitcoin script & use max value if not set for btc

--- scripts/bitcoin/createBackupBitcoin.ts
+++ scripts/bitcoin/createBackupBitcoin.ts
@@ -1,48 +1,50 @@
-import { Psbt } from "bitcoinjs-lib/src/psbt";
-import * as bip39 from "bip39";
-const privateKey = process.env.PRIVATE_KEY;
-const seedPhrase = process.env.MNEMONIC;
-const recipient = process.env.RECIPIENT_ADDRESS_BTC; // recipient of the funds (if invoked), this could be your exchange account for example
-const notValidBefore = process.env.NOT_VALID_BEFORE; // timestamp specifying when this transaction becomes valid
-const value = parseInt(process.env.VALUE_BTC as string); // owner of funds
-const bip65 = require('bip65');
-import BIP32Factory from 'bip32';
-import ECPairFactory from 'ecpair';
-import * as ecc from 'tiny-secp256k1';
-import { p2sh, p2wpkh } from "bitcoinjs-lib/src/payments";
-const ECPair = ECPairFactory(ecc);
-const request = require("superagent");
-const bip32 = BIP32Factory(ecc);
+import { PrivateKey, Transaction } from "bitcore-lib";
+const privateKey = process.env.PRIVATE_KEY_BTC; // TODO add mnemonic support
+const recipient = process.env.RECIPIENT_ADDRESS_BTC;
+const notValidBefore = process.env.NOT_VALID_BEFORE;
+import * as bitcore from "bitcore-lib";
+import * as request from "superagent";
+import UnspentOutput = Transaction.UnspentOutput;
 
 async function main() {
-    let wallet;
-    if(seedPhrase !== "") {
-        const seed = await bip39.mnemonicToSeed(seedPhrase as string);
-        wallet = bip32.fromSeed(seed);
-    } else {
-        wallet = ECPair.fromPrivateKey(Buffer.from(privateKey as string, "hex"));
-    }
-    const pubkey = wallet.publicKey;
-    const address = p2sh({redeem: p2wpkh({ pubkey })}).address;
-    const inputs = await getUnspentInputs(address as string);
-    const lockTime = bip65.encode({ utc: parseInt(notValidBefore as string) });
-    const tx = new Psbt()
-        .addInputs(inputs)
-        .addOutput({ address: recipient as string, value: value })
-        .setLocktime(lockTime)
-        .signAllInputs(wallet);
+    const wallet = new bitcore.PrivateKey(privateKey);
+    const address = wallet.toAddress().toString();
+    const inputs = await getUnspentInputs(address);
+    const value = getValue(inputs);
+    const tx = new bitcore.Transaction()
+        .from(inputs)
+        .to(recipient as string, value)
+        .lockUntilDate(parseInt(notValidBefore as string))
+        .sign(wallet as PrivateKey);
 
-    return tx.finalizeAllInputs().extractTransaction().toHex();
+    return tx.toString();
+}
+
+function getValue(inputs: UnspentOutput[]) {
+    let value = 0;
+    // if not set in .env, send max
+    if(process.env.VALUE_BTC === "") {
+        inputs.map((i) => {
+            value += i.satoshis;
+        });
+    } else {
+        value = parseInt(process.env.VALUE_BTC as string);
+    }
+
+    return value;
 }
 
 async function getUnspentInputs(address: string) {
     const data = await request.get(`https://blockchain.info/unspent?active=${address}`);
 
-    return data.body.unspent_outputs.map((tx) => {
-        return {
-            hash: tx.tx_hash,
-            index: 1 //tx.tx_index, // TODO check this
-        }
+    return data.body.unspent_outputs.map((tx: { tx_hash: any; tx_output_n: any; script: any; value: any; }) => {
+        return new UnspentOutput({
+            "txid" : tx.tx_hash,
+            "vout" : tx.tx_output_n,
+            "address" : address,
+            "scriptPubKey" : tx.script,
+            "satoshis" : tx.value
+        })
     });
 }
 
